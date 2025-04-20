286ae7efaa3dd381846205bb3d543f52e0f4c424
keetsm1 <145614621+keetsm1@users.noreply.github.com>
2025-04-20 00:49:21 -0400

    Added new game UI, select a team and routes for the new gamee button in home page

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -4,3 +4,12 @@ from app import app  # this imports the app object created in __init__.py
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/newGame')
+def newGame():
+    return render_template('newGame.html')
+
+@app.route('/startGame', methods= ['POST'])
+def startGame():
+    team_name= request.form[team];
+    return render_template('game.html', team= team_name);
