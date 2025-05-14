e204f363d11a8b9803534dcc157cd55134352343
keetsm1 <145614621+keetsm1@users.noreply.github.com>
2025-05-14 13:15:14 -0400

    Sidebar and fixed db

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -3,7 +3,9 @@ from app import app  # this imports the app object created in __init__.py
 from app.game_logic import player_gen
 # from app.game_logic.sqldb import reset_database
 app.secret_key= "kobe"
+from app.game_logic.sqldb import conn
 
+print(app.url_map)
 
 @app.route('/')
 def home():
@@ -26,6 +28,48 @@ def sidebar_home():
     team_name= session.get('team_name','Default Team')
     return render_template('game.html', team=team_name)
 
-@app.route('/rosters')
+@app.route('/rosters', methods=['GET'])
 def rosters():
-    return render_template('roster.html')
+    current_team_name= session.get('team_name',None)
+
+    selected_team= request.args.get('team', current_team_name)
+
+    cur=conn.cursor()
+
+    cur.execute("SELECT DISTINCT team FROM players ORDER by team;")
+
+    teams= [row[0] for row in cur.fetchall()]
+
+    if not selected_team and teams:
+        selected_team = teams[0]
+
+    cur.execute("""
+            SELECT name, position
+              FROM players
+             WHERE team = %s
+          ORDER BY
+            CASE WHEN position = 'G' THEN 3
+                 WHEN position = 'D' THEN 2
+                 ELSE 1
+            END,
+            name;
+        """, (selected_team,))
+    players = cur.fetchall()
+    cur.close()
+
+    roster = {'Forwards': [], 'Defense': [], 'Goalies': []}
+    for name, position in players:
+        if position == 'G':
+            roster['Goalies'].append(name)
+        elif position in ('LD', 'RD', 'LD/RD'):
+            roster['Defense'].append(name)
+        else:
+            roster['Forwards'].append(name)
+
+    return render_template(
+        'roster.html',
+        teams=teams,
+        selected_team=selected_team,
+        roster=roster
+    )
+
